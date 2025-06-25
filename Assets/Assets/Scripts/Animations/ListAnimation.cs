62fc5969a363ff441e95257a1fea123556136f07
ElliotDev <100314844+AtraxxDev@users.noreply.github.com>
2025-06-25 13:23:22 -0600

    Add list animation

--- Assets/Assets/Scripts/Animations/ListAnimation.cs
+++ Assets/Assets/Scripts/Animations/ListAnimation.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 
 public class ListAnimation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    /*[Title("Position Data")]
+    [SerializeField] private Vector3 closePosition;
+    [SerializeField] private Vector3 midPosition;
+    [SerializeField] private Vector3 openPosition;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float duration = 0.3f;
+    [SerializeField] private Ease easeType = Ease.OutQuad;*/
+
+    [SerializeField] private Animator animator;
+
+    [ReadOnly, ShowInInspector]
+    private bool isOpen = false;
+
+    [Button]
+    public void ToggleList()
     {
-        
+        isOpen = !isOpen;
+
+        if (animator != null)
+        {
+            animator.SetBool("isOpen", isOpen);
+        }
+
+        /*Vector3 startPos = transform.localPosition;
+        Vector3 midPos = midPosition;
+        Vector3 endPos = isOpen ? openPosition : closePosition;
+
+ 
+
+        Sequence parabolaSeq = DOTween.Sequence();
+
+        parabolaSeq.Append(transform.DOLocalMove(midPos, duration / 2f)
+                                    .SetEase(easeType));
+        parabolaSeq.Append(transform.DOLocalMove(endPos, duration / 2f)
+                                    .SetEase(easeType));*/
     }
 }
